5307c80d61593aaa1c5795b8fc528894bded2887
coverza <52163631+coverza@users.noreply.github.com>
2019-07-04 12:38:18 +0300

    Add date to order

--- contact.php
+++ contact.php
@@ -1,74 +1,37 @@
 <?php
-
+// Отправка заявки с контактов
 $post = (!empty($_POST)) ? true : false;
 
 $check = $_POST['check'];
+$error = 'Что-то пошло не так!';
 
 if($post && $check=='secretcode')
 {
-$email = trim($_POST['email']);
-$name = htmlspecialchars($_POST['name']);
-$email = htmlspecialchars($_POST['mail']);
-$message = htmlspecialchars($_POST['message']);
-$error = '';
+    $email = trim($_POST['email']);
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['mail']);
+    $message = htmlspecialchars($_POST['message']);
+    $date = htmlspecialchars($_POST['date']);
 
-if(!$name)
-{
-$error .= 'Пожалуйста введите ваше имя<br />';
-}
+    $name_tema = "=?utf-8?b?". base64_encode($name) ."?=";
 
-// Проверка телефона
-function ValidateTel($valueTel)
-{
-$regexTel = "/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/";
-if($valueTel == "") {
-return false;
-} else {
-$string = preg_replace($regexTel, "", $valueTel);
-}
-return empty($string) ? true : false;
-}
-if(!$email)
-{
-$error .= "Пожалуйста введите email<br />";
-}
-if($email && !ValidateTel($email))
-{
-$error .= "Введите корректный email<br />";
-}
-if(!$error)
+    $subject ="Новая заявка с сайта coverzaband.ru";
+    $subject1 = "=?utf-8?b?". base64_encode($subject) ."?=";
+    $message1 ="\n\nИмя: ".$name."\n\nE-mail: " .$email."\n\nДата мероприятия: " .$date."\n\nСообщение: ".$message."\n\n";	
 
-// (length)
-if(!$message || strlen($message) < 1)
-{
-$error .= "Введите ваше сообщение<br />";
-}
-if(!$error)
-{
+    $header = "Content-Type: text/plain; charset=utf-8\n";
 
+    $header .= "From: Новая заявка <info@coverza.ru>\n\n";	
+    $mail = mail("info@coverza.ru", $subject1, iconv ('utf-8', 'windows-1251', $message1), iconv ('utf-8', 'windows-1251', $header));
 
-$name_tema = "=?utf-8?b?". base64_encode($name) ."?=";
-
-$subject ="Новая заявка с сайта coverzaband.ru";
-$subject1 = "=?utf-8?b?". base64_encode($subject) ."?=";
-$message1 ="\n\nИмя: ".$name."\n\nE-mail: " .$email."\n\nСообщение: ".$message."\n\n";	
-
-$header = "Content-Type: text/plain; charset=utf-8\n";
-
-$header .= "From: Новая заявка <info@coverza.ru>\n\n";	
-$mail = mail("info@coverza.ru", $subject1, iconv ('utf-8', 'windows-1251', $message1), iconv ('utf-8', 'windows-1251', $header));
-
-if($mail)
-{
-header ('Location: spasibo');
-exit();
-}
-
+    if($mail)
+    {
+        header ('Location: spasibo');
+        exit();
+    }
 }
 else
 {
-echo '<div class="notification_error">'.$error.'</div>';
-}
-
+    echo '<div class="notification_error">'.$error.'</div>';
 }
 ?>
--- send.php
+++ send.php
@@ -1,50 +1,38 @@
 <?php
-
+// Отправка заявки с главной
 $post = (!empty($_POST)) ? true : false;
+$error = 'Что-то пошло не так!';
 
 if($post)
 {
 
-$name = htmlspecialchars($_POST['name']);
-$tariff = htmlspecialchars($_POST['check']);
-$tel = htmlspecialchars($_POST["phone"]);
-$error = '';
+    $name = htmlspecialchars($_POST['name']);
+    $date = htmlspecialchars($_POST['date']);
+    $tariff = htmlspecialchars($_POST['check']);
+    $tel = htmlspecialchars($_POST["phone"]);
 
-if(!$name)
-{
-$error .= 'Пожалуйста введите ваше имя<br />';
-}
+    $name_tema = "=?utf-8?b?". base64_encode($name) ."?=";
 
-if(!$error)
-{
+    $subject ="Новая заявка с сайта coverzaband.ru";
+    $subject1 = "=?utf-8?b?". base64_encode($subject) ."?=";
 
-
-$name_tema = "=?utf-8?b?". base64_encode($name) ."?=";
-
-$subject ="Новая заявка с сайта coverzaband.ru";
-$subject1 = "=?utf-8?b?". base64_encode($subject) ."?=";
-/*
-$message ="\n\nСообщение: ".$message."\n\nИмя: " .$name."\n\nТелефон: ".$tel."\n\n";
-*/
-$message1 ="\n\nИмя: ".$name."\n\nТелефон: " .$tel."\n\nТариф: " .$tariff."\n\n";	
+    $message1 ="\n\nИмя: ".$name."\n\Дата мероприятия: " .$date."\n\nТелефон: " .$date."\n\nТариф: " .$tariff."\n\n";	
 
 
-$header = "Content-Type: text/plain; charset=utf-8\n";
+    $header = "Content-Type: text/plain; charset=utf-8\n";
 
-$header .= "From: Новая заявка с сайта <coverzaband@gmail.com>\n\n";	
-$mail = mail("coverzaband@gmail.com", $subject1, iconv ('utf-8', 'windows-1251', $message1), iconv ('utf-8', 'windows-1251', $header));
+    $header .= "From: Новая заявка с сайта <coverzaband@gmail.com>\n\n";	
+    $mail = mail("coverzaband@gmail.com", $subject1, iconv ('utf-8', 'windows-1251', $message1), iconv ('utf-8', 'windows-1251', $header));
 
-if($mail)
-{
-header ('Location: spasibo');
-exit();
-}
+    if($mail)
+    {
+        header ('Location: spasibo');
+        exit();
+    }
 
 }
 else
 {
-echo '<div class="notification_error">'.$error.'</div>';
-}
-
+    echo '<div class="notification_error">'.$error.'</div>';
 }
 ?>
